fb2d68f15a7117fdafb9372422e32704c3bbe7ae
Vivek Raman <VR.AC4BF@live.com>
2018-10-10 17:18:16 +0530

    Created framework for GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-// struct
 
 public class GameManager : MonoBehaviour {
 
@@ -10,12 +9,22 @@ public class GameManager : MonoBehaviour {
     private GameObject CellPrefab;
     [SerializeField]
     private GameObject GridParentPrefab;
-    
-    public Dictionary<GameObject, float> Grid = new Dictionary<GameObject, float>();
+
+    public List<GridClass> Grid = new List<GridClass>();
+    private int TileCount = 0;
 
     private void Start()
     {
         SpawnGrid();
+        BeginGame();
+
+    }
+
+    private void Update()
+    {
+        ProcessInput(GetInputDirection());
+        
+        
     }
 
     // Spawns grid column-wise
@@ -35,29 +44,83 @@ public class GameManager : MonoBehaviour {
         }
 
         // Column 1
-        Grid.Add(Instantiate(CellPrefab, new Vector3(-1.50f, +1.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(-1.50f, +0.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(-1.50f, -0.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(-1.50f, -1.50f), Quaternion.identity, GridParent.transform), 0f);
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(-1.50f, +1.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(-1.50f, +1.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(-1.50f, +0.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(-1.50f, +0.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(-1.50f, -0.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(-1.50f, -0.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(-1.50f, -1.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(-1.50f, -1.50f)));
 
         // Column 2
-        Grid.Add(Instantiate(CellPrefab, new Vector3(-0.50f, +1.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(-0.50f, +0.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(-0.50f, -0.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(-0.50f, -1.50f), Quaternion.identity, GridParent.transform), 0f);
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(-0.50f, +1.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(-0.50f, +1.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(-0.50f, +0.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(-0.50f, +0.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(-0.50f, -0.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(-0.50f, -0.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(-0.50f, -1.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(-0.50f, -1.50f)));
 
         // Column 3
-        Grid.Add(Instantiate(CellPrefab, new Vector3(+0.50f, +1.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(+0.50f, +0.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(+0.50f, -0.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(+0.50f, -1.50f), Quaternion.identity, GridParent.transform), 0f);
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(+0.50f, +1.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(+0.50f, +1.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(+0.50f, +0.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(+0.50f, +0.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(+0.50f, -0.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(+0.50f, -0.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(+0.50f, -1.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(+0.50f, -1.50f)));
 
         // Column 4
-        Grid.Add(Instantiate(CellPrefab, new Vector3(+1.50f, +1.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(+1.50f, +0.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(+1.50f, -0.50f), Quaternion.identity, GridParent.transform), 0f);
-        Grid.Add(Instantiate(CellPrefab, new Vector3(+1.50f, -1.50f), Quaternion.identity, GridParent.transform), 0f);
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(+1.50f, +1.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(+1.50f, +1.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(+1.50f, +0.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(+1.50f, +0.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(+1.50f, -0.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(+1.50f, -0.50f)));
+        Grid.Add(new GridClass(Instantiate(CellPrefab, new Vector3(+1.50f, -1.50f), Quaternion.identity, GridParent.transform), 0, new Vector3(+1.50f, -1.50f)));
     }
 
-    
+    // Spawn two tiles at random
+    public void BeginGame()
+    {
+        // Clear all tiles
+        // Add two tiles
+    }
+
+    // Adds a tile
+    public void AddTile()
+    {
+
+        TileCount++;
+        if (IsBoardFull())
+            Debug.Log("Game over!");
+    }
+
+    // Returns direction of swipe gesture
+    public Vector3 GetInputDirection()
+    {
+        return Vector3.zero;
+    }
+
+    // Runs loop to process every tile
+    public void ProcessInput(Vector3 Input)
+    {
+
+        // Add tile at the end of processing
+        AddTile();
+    }
+
+    // Combines two like tiles
+    public void Combine(int FromIndex, int ToIndex)
+    {
+        if (FromIndex == ToIndex)
+            return;
+        TileCount--;
+        if (Is2048())
+            Debug.Log("Congratulations!");
+    }
+
+    // Check win condition 2048 tile
+    public bool Is2048()
+    {
+        bool bIs2048 = false;
+
+        return bIs2048;
+    }
+
+    public bool IsBoardFull()
+    {
+        if (TileCount >= 16)
+            return true;
+        return false;
+    }
 }
+
---
+++ Assets/Scripts/GridClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridClass : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Tile;
+    private Vector3 Center;
+    private float Value = 0;
+
+    public GridClass()
+    {
+        Value = 0f;
+    }
+
+    public GridClass(GameObject tile, int value, Vector3 center)
+    {
+        Tile = tile;
+        Value = value;
+        Center = center;
+    }
+
+    public Vector3 GetLocation() { return Center; }
+}
